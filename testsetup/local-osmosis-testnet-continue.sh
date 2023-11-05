328f814a118191922cca647dd806083e1709eb02
Fabian Weber <frznhope@gmail.com>
2023-11-05 19:12:03 +0100

    fix port on osmosis as well

--- testsetup/local-osmosis-testnet-continue.sh
+++ testsetup/local-osmosis-testnet-continue.sh
@@ -5,6 +5,6 @@ set -e
 killall osmosisd || true
 
 # start all three validators
-tmux new -s osmosisvalidator1 -d osmosisd start --home=$HOME/.osmosisd/validator1
+tmux new -s osmosisvalidator1 -d osmosisd start --home=$HOME/.osmosisd/validator1 --rpc.laddr tcp://0.0.0.0:26657
 tmux new -s osmosisvalidator2 -d osmosisd start --home=$HOME/.osmosisd/validator2
 tmux new -s osmosisvalidator3 -d osmosisd start --home=$HOME/.osmosisd/validator3
