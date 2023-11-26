4d72f5b3fd34a6cca7f3a2b91bafdde5483f2f82
Fabian Weber <frznhope@gmail.com>
2023-11-26 11:06:49 +0200

    added safe guard for polling

--- proxy/polling.js
+++ proxy/polling.js
@@ -4,30 +4,33 @@ import axios from "axios";
 export const subscribe = (network, cb) => {
   let lastHeight = 0;
   setInterval(async () => {
-    let rpc = network.rpc;
-    let {
-      data: {
-        result: { block },
-      },
-    } = await axios.get(rpc + "/block");
-    let height = block.header.height;
-    if (height > lastHeight) {
-      lastHeight = height;
-
+    try {
+      let rpc = network.rpc;
       let {
         data: {
-          result: { txs },
+          result: { block },
         },
-      } = await axios.get(
-        rpc +
-          `/tx_search?query="wasm._contract_address='${network.contract}' AND tx.height=${height}"`
-      );
+      } = await axios.get(rpc + "/block");
+      let height = block.header.height;
+      if (height > lastHeight) {
+        lastHeight = height;
 
-      if (txs.length > 0) {
-        console.log("Relevant block detected", network.id);
-        cb(height);
+        let {
+          data: {
+            result: { txs },
+          },
+        } = await axios.get(
+          rpc +
+            `/tx_search?query="wasm._contract_address='${network.contract}' AND tx.height=${height}"`
+        );
+
+        if (txs.length > 0) {
+          console.log("Relevant block detected", network.id);
+          cb(height);
+        }
       }
+    } catch (err) {
+      console.error(err);
     }
-    cb();
   }, 5000);
 };
