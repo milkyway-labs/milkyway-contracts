22fef35f9410ebaf414ab770285def5088337848
Manuel <manuel@milkyway.zone>
2024-05-31 14:02:57 +0200

    refactor: apply coderabit suggestion

--- contracts/treasury/src/helpers.rs
+++ contracts/treasury/src/helpers.rs
@@ -1,13 +1,10 @@
 use cosmwasm_std::{Addr, StdError, StdResult};
 
 pub fn validate_address(address: &String, prefix: &str) -> StdResult<Addr> {
-    let validated_addr = bech32::decode(address);
+    let validated_addr =
+        bech32::decode(address).map_err(|_| StdError::generic_err("Invalid address"))?;
 
-    if validated_addr.is_err() {
-        return Err(StdError::generic_err("Invalid address"));
-    }
-
-    if validated_addr.unwrap().0 != prefix {
+    if validated_addr.0 != prefix {
         return Err(StdError::generic_err("Invalid address prefix"));
     }
 
